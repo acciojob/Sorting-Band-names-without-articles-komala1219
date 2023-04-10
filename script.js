4f6d864e6046b89863cc6e0b5d33f828b62a1750
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-10 17:35:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 function strip (word) {
     let regex = new RegExp('^(a |the |an )', 'i')
     return word.replace(regex, '').trim()
