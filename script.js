458f123fbe6b253b279e4af101efc5efd22e3c40
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-10 17:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function strip (word) {
     return word.replace(regex, '').trim()
 }
 
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+const sortedBands = touristSpots .sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
 
 
 /** 
